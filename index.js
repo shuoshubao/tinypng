f5d4138835585cb78b2568bf37c4126890c7a26f
yangting <yangting@lianjia.com>
2017-12-08 16:25:39 +0800

    modifyindex.js

--- index.js
+++ index.js
@@ -3,5 +3,58 @@ const tinify = require('tinify')
 
 tinify.key = 'KckuU929qtv_nPK_czL6HKfcAJO9FCKm'
 
+fs.readdir('./img/src', function(err, files) {
+  files.shift();
+  console.log(files)
+  if(err) {
+    return console.error(err);
+  }
+  files.forEach(function(file) {
+    if(fs.statSync('./img/' + file).isDirectory()) {
+      fs.readdir('./img/' + file + '/', function(err, files2) {
+        if(err) {
+          return console.error(err);
+        }
+        files2.forEach(function(file2) {
+          fs.readFile('./img/' + file + '/' + file2, function(err, sourceData) {
+            if(err) {
+              throw err;
+            }
+            tinify.fromBuffer(sourceData).toBuffer(function(err, resultData) {
+              if(err) {
+                throw err;
+              }
+              fs.open('./img2/' + file + '/' + file2, 'w', function(err) {
+                if (err) {
+                  return console.error(err);
+                }
+                fs.writeFileSync('./img2/' + file + '/' + file2, resultData);
+                console.log('创建成功！')
+              });
+            });
+          })
+        })
+      })
+    }else {
+      fs.readFile('./img/' + file, function(err, sourceData) {
+        if(err) {
+          throw err;
+        }
+        tinify.fromBuffer(sourceData).toBuffer(function(err, resultData) {
+          if(err) {
+            throw err;
+          }
+          fs.open('./img2/'+ file, 'w', function(err) {
+            if (err) {
+              return console.error(err);
+            }
+            fs.writeFileSync('./img2/'+ file, resultData);
+            console.log('创建成功！')
+          });
+        })
+      })
+    }
+  })
+});
 
 
